944209ccef1b81a7d0feb2110b24678a50af5121
SolopiyMukola <solopiy@i.ua>
2019-11-28 14:12:51 +0200

    delete files

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-const group = "Hi Group";
-console.log(group); // Ololo coment
-
-
-/*
-    Some comments
-*/
--- second lesson/homework/homework1.js
+++
@@ -1,9 +0,0 @@
-// Exercise. 
-// Need to change the order of result variable "a" and "b" using only one additional variable
-let a = 'green';
-let b = 'red';
-
-// Your code
-
-console.log(a) // Should equal 'red'
-console.log(b) // Should equal 'green'
